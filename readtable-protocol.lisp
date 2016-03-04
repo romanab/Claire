763f8c74c1387f2f46198ddf19ad76a201477dd7
Robert Strandh <robert.strandh@gmail.com>
2016-03-04 09:04:24 +0100

    Add definition of generic function GET-MACRO-CHARACTER.

--- readtable-protocol.lisp
+++ readtable-protocol.lisp
@@ -3,3 +3,7 @@
 ;;; Protocol class READTABLE. This class should not be instantiated
 ;;; directly.
 (defclass readtable () ())
+
+;;; This function is a generic version of the Common Lisp function
+;;; GET-MACRO-CHARACTER.
+(defgeneric get-macro-character (readtable character))
