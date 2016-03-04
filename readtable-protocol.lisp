6673beda68deeab5cbd3f1add1214c54cde50dc6
Robert Strandh <robert.strandh@gmail.com>
2016-03-04 09:04:36 +0100

    Add definition of generic function SET-MACRO-CHARACTER.

--- readtable-protocol.lisp
+++ readtable-protocol.lisp
@@ -7,3 +7,8 @@
 ;;; This function is a generic version of the Common Lisp function
 ;;; GET-MACRO-CHARACTER.
 (defgeneric get-macro-character (readtable character))
+
+;;; This function is a generic version of the Common Lisp function
+;;; SET-MACRO-CHARACTER.
+(defgeneric set-macro-character
+    (readtable character function &optional non-terminating-p))
