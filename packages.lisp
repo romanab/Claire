e2fa1e7acbf2d142f7106dd373da868b0f2d0afb
Robert Strandh <robert.strandh@gmail.com>
2016-03-04 08:58:15 +0100

    Shadow the names of some symbols from the CL package so that we can reuse those names here.

--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,11 @@
 
 (defpackage #:claire
   (:use #:common-lisp)
+  (:shadow #:readtable
+	   #:get-macro-character
+	   #:set-macro-character
+	   #:make-dispatch-macro-character
+	   #:get-dispatch-macro-character
+	   #:set-dispatch-macro-character
+	   #:copy-readtable)
   (:export))
