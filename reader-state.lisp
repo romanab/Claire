71c2d8f329eb8cd1b63130d7af4cc8d2dbd7984f
Robert Strandh <robert.strandh@gmail.com>
2016-03-08 15:22:18 +0100

    New class representing state 2 of the reader algorithm.

--- reader-state.lisp
+++ reader-state.lisp
@@ -10,3 +10,15 @@
 ;;; performed, or a character is read and a new state is entered
 ;;; according to the syntax type of that character.
 (defclass reader-state-1 (reader-state) ())
+
+;;; According to section 2.2 of the Common Lisp HyperSpec, state 2 is
+;;; entered only from state 1 when an invalid character is read.
+;;; According that that section, in this state, an error of type
+;;; reader-error is signaled.  However, such an error is signaled in
+;;; state 8 and state 9, also when an invalid character is read.  It
+;;; might be more useful then to enter state 2 from states 8 and 9 in
+;;; this situation, rather than duplicating the code that signals an
+;;; error.  That way, client code need only define one method for the
+;;; processing of an invalid character.
+(defclass reader-state-2 (reader-state)
+  ((%invalid-character :initarg :invalid-character :reader invalid-character)))
