37489584d88f6300d7f5f99457327481f6ec036b
Robert Strandh <robert.strandh@gmail.com>
2016-03-09 17:43:40 +0100

    Reuse a single state instance for state 3 rather than allocating a new one each time.

--- default-reader-algorithm.lisp
+++ default-reader-algorithm.lisp
@@ -3,6 +3,9 @@
 (defparameter *reader-state-2*
   (make-instance 'reader-state-2))
 
+(defparameter *reader-state-3*
+  (make-instance 'reader-state-3))
+
 (defmethod reader-step ((readtable standard-readtable)
 			(state reader-state-1)
 			(syntax end-of-file))
@@ -16,8 +19,7 @@
 (defmethod reader-step ((readtable standard-readtable)
 			(state reader-state-1)
 			(syntax whitespace))
-  (setf *current-reader-state*
-	(make-instance 'reader-state-3)))
+  (setf *current-reader-state* *reader-state-3*))
 
 (defmethod reader-step ((readtable standard-readtable)
 			(state reader-state-1)
