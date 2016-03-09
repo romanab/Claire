f0fb43522a744876029cccdd01b93d2ab0e6538a
Robert Strandh <robert.strandh@gmail.com>
2016-03-09 17:16:50 +0100

    Define method on READER-STEP specialized to state 1 and INVALID syntax type.

--- default-reader-algorithm.lisp
+++ default-reader-algorithm.lisp
@@ -4,3 +4,9 @@
 			(state reader-state-1)
 			(syntax end-of-file))
   (error 'reader-error))
+
+(defmethod reader-step ((readtable standard-readtable)
+			(state reader-state-1)
+			(syntax invalid))
+  (setf *current-reader-state*
+	(make-instance 'reader-state-2)))
